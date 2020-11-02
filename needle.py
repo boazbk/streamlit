1b613ed8d46bfba2e8aeda4b60e1ed1e09d5b513
Boaz Barak <b@boazbarak.org>
2020-11-02 17:14:54 -0500

    update app

--- needle.py
+++ needle.py
@@ -10,8 +10,11 @@ st.title('State to national needle')
 
 '''
 Data from [538 interactive forecast](https://projects.fivethirtyeight.com/trump-biden-election-map/) (see [this file](https://projects.fivethirtyeight.com/trump-biden-election-map/simmed-maps.json))
+
+Choose below the range of possible margins for Biden in North Carolina, Florida, and Georgia, and see how it changes the predicted chance of victory.*
 '''
 
+st.text("Disclaimer: This may not 100% correspond to 538's simulator because:\n(1) we assume the margin is always between -10 and +10, \n(2) their data may be more updated, and\n(3) I may have bugs")
 
 @st.cache
 def get_data():
@@ -40,12 +43,12 @@ fullnames = { "NC": "North Carolina" , "FL": "Florida", "GA": "Georgia" }
 
 bounds = { s+mod: -10 if mod==" (min)" else +10 for s in states for mod in [" (min)"," (max)"] }
 
-sliders = { s: st.slider(fullnames[s], -10.0,+10.0, (-10.0,+10.0),0.5)  for s in states }
+sliders = { s: st.slider(fullnames[s], -10.0,+10.0, (-10.0,+10.0),0.5, format="%.1f")  for s in states }
 
 
 temp = df.copy()
 for s in states:
-    temp = temp[(-temp[s]> sliders[s][0]) & (-temp[s]< sliders[s][1])]
+    temp = temp[(-temp[s]>= sliders[s][0]) & (-temp[s]< sliders[s][1])]
 
 '''
 ### Prediction
@@ -62,7 +65,7 @@ ax.set_xticks(bins)
 # add a 'best fit' line
 ax.set_xlabel('Biden electoral votes', fontsize= 24)
 ax.set_ylabel('Probability')
-cond = ", ".join([s +f"$\in [{sliders[s][0]},{sliders[s][1]}]$" for s in states])
+cond = ", ".join([s +f"$\in [{sliders[s][0]},{sliders[s][1]})$" for s in states])
 
 plt.suptitle("Histogram of Biden electoral votes", color="red", fontsize=24)
 plt.title(rf'Assuming {cond}')
